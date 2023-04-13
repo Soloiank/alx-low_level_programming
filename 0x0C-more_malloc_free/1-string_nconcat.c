e764df711f59b28e00b4eee3a44ca40e22d73df0
Soloiank <biisalamon@gmail.com>
2023-04-13 04:18:24 -0700

    more malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,49 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * *string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: limit of s2
- * Return: pointer to new space in memory or null
- **/
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *comb;
-	int i;
-	unsigned int j;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	i = 0;
-	while (s1[i])
-		i++;
-	i++;
-	comb = malloc(sizeof(*comb) * (i + n));
-	if (comb == NULL)
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		comb[i] = s1[i];
-		i++;
-	}
-	for (j = 0; j < n && s2[j] != '\0'; j++)
-	{
-		comb[i] = s2[j];
-		i++;
-	}
-	comb[i] = '\0';
-	return (comb);
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
